34ae89b4e68c33197de575502add8eacf7c033e8
ETGit39 <eliza.tom@dubizzle.com>
2023-10-28 01:31:38 +0400

    yet again

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,13 @@
 "use strict";
+const core = require("@actions/core");
+const { App, LogLevel } = require("@slack/bolt");
+const { WebClient } = require("@slack/web-api");
 
-const { App, LogLevel } = require('@slack/bolt');
-const { WebClient } = require('@slack/web-api');
-const crypto = require('crypto');
-const core = require('@actions/core');
+function generateRandomToken() {
+    return Math.random().toString(36).substr(2, 10); // Generate a random alphanumeric token
+}
+
+const uniqueToken = generateRandomToken();
 
 const token = process.env.SLACK_BOT_TOKEN || "";
 const signingSecret = process.env.SLACK_SIGNING_SECRET || "";
@@ -11,9 +15,6 @@ const slackAppToken = process.env.SLACK_APP_TOKEN || "";
 const channel_id = process.env.SLACK_CHANNEL_ID || "";
 const environment = process.env.ENVIRONMENT || "";
 const url = process.env.URL || "";
-const sign_token = crypto.randomBytes(16).toString('hex');
-let waitingForResponse = true;
-
 const app = new App({
     token: token,
     signingSecret: signingSecret,
@@ -23,144 +24,145 @@ const app = new App({
     logLevel: LogLevel.DEBUG,
 });
 
-async function run() {
-    try {
-        const web = new WebClient(token);
-        const github_server_url = process.env.GITHUB_SERVER_URL || "";
-        const github_repos = process.env.GITHUB_REPOSITORY || "";
-        const run_id = process.env.GITHUB_RUN_ID || "";
-        const actionsUrl = `${github_server_url}/${github_repos}/actions/runs/${run_id}`;
-        const runnerOS = process.env.RUNNER_OS || "";
-        const actor = process.env.USER_ID || "";
+function run() {
+    return (async () => {
+        try {
+            const web = new WebClient(token);
+            const github_server_url = process.env.GITHUB_SERVER_URL || "";
+            const github_repos = process.env.GITHUB_REPOSITORY || "";
+            const run_id = process.env.GITHUB_RUN_ID || "";
+            const actionsUrl = `${github_server_url}/${github_repos}/actions/runs/${run_id}`;
+            const runnerOS = process.env.RUNNER_OS || "";
+            const actor = process.env.USER_ID || "";
 
-        async function sendApprovalMessage() {
+            // Include the unique token in the Slack message
             await web.chat.postMessage({
                 channel: channel_id,
                 text: "GO TO PRODUCTION?",
                 blocks: [
                     {
-                        type: "section",
-                        text: {
-                            type: "mrkdwn",
-                            text: `Hey <@${actor}> 👋 Everything looks good! Do you want to go to prod? \nChoose "Go" when you are ready to release to production. Make sure your PR has all required approvals and can be merged. \nIf you choose "Stop", the pipeline will stop.`,
-                        }
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": `Hey <@${actor}> 👋 Everything looks good! Do you want to go to prod? \nChoose "Go" when you are ready to release to production. Make sure your PR has all required approvals and can be merged. \nIf you choose "Stop" , the pipeline will stop.`,
+                        },
                     },
                     {
-                        type: "section",
-                        fields: [
+                        "type": "section",
+                        "fields": [
                             {
-                                type: "mrkdwn",
-                                text: `*Actions URL:*\n${actionsUrl}`
+                                "type": "mrkdwn",
+                                "text": `*Actions URL:*\n${actionsUrl}`,
                             },
-                            {
-                                type: "mrkdwn",
-                                text: `*Token:*\n${sign_token}`
-                            }
-                        ]
+                        ],
                     },
-                    // Add a button block for user response
                     {
-                        type: "actions",
-                        elements: [
+                        "type": "context",
+                        "elements": [
                             {
-                                type: "button",
-                                text: {
-                                    type: "plain_text",
-                                    text: "Approve",
+                                "type": "mrkdwn",
+                                "text": `Token: ${uniqueToken}`, // Include the unique token
+                            },
+                        ],
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "emoji": true,
+                                    "text": "Go",
                                 },
-                                style: "primary",
-                                action_id: "slack-approval-approve",
+                                "style": "primary",
+                                "value": "approve",
+                                "action_id": "slack-approval-approve",
                             },
                             {
-                                type: "button",
-                                text: {
-                                    type: "plain_text",
-                                    text: "Reject",
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "emoji": true,
+                                    "text": "Stop",
                                 },
-                                style: "danger",
-                                action_id: "slack-approval-reject",
+                                "style": "danger",
+                                "value": "reject",
+                                "action_id": "slack-approval-reject",
                             },
                         ],
                     },
                 ],
             });
-        }
 
-        // Function to check for approval response
-        function checkForApprovalResponse() {
-            return new Promise((resolve) => {
-                app.action('slack-approval-approve', async ({ ack, client, body, logger }) => {
-                    await ack();
-
-                    if (waitingForResponse) {
-                        const response_blocks = body.message?.blocks;
-                        const responseToken = extractTokenFromBlocks(response_blocks);
-
-                        if (responseToken === sign_token) {
+            app.action("slack-approval-approve", ({ ack, client, body, logger }) => {
+                const responseToken = body.message.blocks.find(
+                    (block) =>
+                        block.type === "context" &&
+                        block.elements[0].text.includes("Token:")
+                );
+                if (responseToken) {
+                    const receivedToken = responseToken.elements[0].text
+                        .split("Token: ")[1]
+                        .trim();
+                    if (receivedToken === uniqueToken) {
+                        ack();
+                        try {
+                            const response_blocks = body.message.blocks;
                             response_blocks.pop();
                             response_blocks.push({
-                                type: 'section',
+                                type: "section",
                                 text: {
-                                    type: 'mrkdwn',
+                                    type: "mrkdwn",
                                     text: `Approved by <@${body.user.id}>`,
                                 },
                             });
-                            await client.chat.update({
-                                channel: body.channel?.id || "",
-                                ts: body.message?.ts || "",
-                                blocks: response_blocks
+                            client.chat.update({
+                                channel: body.channel.id || "",
+                                ts: body.message.ts || "",
+                                blocks: response_blocks,
                             });
-                            process.exit(0);
-                        } else {
-                            console.log('Token does not match. Waiting for another response...');
-                            resolve(checkForApprovalResponse());
+                        } catch (error) {
+                            logger.error(error);
                         }
+                        process.exit(0);
+                    } else {
+                        logger.error("Invalid token in response. Ignoring the response.");
                     }
-                });
+                } else {
+                    logger.error("Token not found in response. Ignoring the response.");
+                }
             });
-        }
 
-        // Function to check for reject response
-        function checkForRejectResponse() {
-            return new Promise((resolve) => {
-                app.action('slack-approval-reject', async ({ ack, client, body, logger }) => {
-                    await ack();
-
-                    if (waitingForResponse) {
-                        const response_blocks = body.message?.blocks;
-                        const responseToken = extractTokenFromBlocks(response_blocks);
-
-                        if (responseToken === sign_token) {
-                            response_blocks.pop();
-                            response_blocks.push({
-                                type: 'section',
-                                text: {
-                                    type: 'mrkdwn',
-                                    text: `Rejected by <@${body.user.id}>`,
-                                },
-                            });
-                            await client.chat.update({
-                                channel: body.channel?.id || "",
-                                ts: body.message?.ts || "",
-                                blocks: response_blocks
-                            });
-                            process.exit(1);
-                        } else {
-                            console.log('Token does not match. Waiting for another response...');
-                            resolve(checkForRejectResponse());
-                        }
-                    }
-                });
+            app.action("slack-approval-reject", ({ ack, client, body, logger }) => {
+                ack();
+                try {
+                    const response_blocks = body.message.blocks;
+                    response_blocks.pop();
+                    response_blocks.push({
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: `Rejected by <@${body.user.id}>`,
+                        },
+                    });
+                    client.chat.update({
+                        channel: body.channel.id || "",
+                        ts: body.message.ts || "",
+                        blocks: response_blocks,
+                    });
+                } catch (error) {
+                    logger.error(error);
+                }
+                process.exit(1);
             });
-        }
 
-        // Call the functions to send the approval message and wait for response
-        await sendApprovalMessage();
-        await Promise.race([checkForApprovalResponse(), checkForRejectResponse()]);
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+            await app.start(3000);
+            console.log("Waiting Approval reaction.....");
+        } catch (error) {
+            if (error instanceof Error) core.setFailed(error.message);
+        }
+    })();
 }
 
 run();
