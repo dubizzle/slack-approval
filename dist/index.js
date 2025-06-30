1488051d335f505224f8aeb048ce044e024a3efa
ETGit39 <eliza.tom@dubizzle.com>
2025-06-30 14:00:48 +0400

    prod cluster scaling approval

--- dist/index.js
+++ dist/index.js
@@ -67,6 +67,8 @@ function run() {
             const actionsUrl = `${github_server_url}/${github_repos}/actions/runs/${run_id}`;
             const runnerOS = process.env.RUNNER_OS || "";
             const actor = process.env.USER_NAME || "";
+            const asg = process.env.asg_names || "";
+            const scaling_direction = process.env.scaling_direction || "";
             (() => __awaiter(this, void 0, void 0, function* () {
                 yield web.chat.postMessage({
                     channel: channel_id,
@@ -76,7 +78,7 @@ function run() {
                             "type": "section",
                             "text": {
                                 "type": "mrkdwn",
-                                "text": `Hey ${actor} 👋 Everything looks good! Do you want to go to prod? \nChoose "Go" when you are ready to release to production. Make sure your PR has all required approvals and can be merged. \nIf you choose "Stop" , the pipeline will stop.`,
+                                "text": `Hey ${actor} 👋 Are you sure you want to scale ${scaling_direction} these ASGs: ${asg} \nChoose "Go" to proceed. \nIf you choose "Stop", the pipeline will terminate.`,
                             }
                         },
                         {
